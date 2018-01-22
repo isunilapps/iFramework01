04aad51aaae98cfc287651da9314d5fda033986b
isunilapps <isunilapps@gmail.com>
2018-01-22 14:33:32 +0530

    #develop project files are added

---
+++ iFramework01/Modules/01/TestClassA.swift
@@ -0,0 +1,44 @@
+//
+//  TestClassA.swift
+//  iFramework01
+//
+//  Created by SUNiL on 1/22/18.
+//  Copyright © 2018 Calibraint. All rights reserved.
+//
+
+import UIKit
+
+public protocol SampleTestDelegate {
+    func delegateMethod01() -> String
+}
+
+open class TestClassA: NSObject, SampleTestDelegate {
+    
+    public static let shared = TestClassA()
+    public var delegate : SampleTestDelegate?
+    
+    public var instanceVariable01 : String = ""
+    
+    override init() {
+        super.init()
+        
+        delegate = self
+    }
+    
+    public func delegateMethod01() -> String {
+        return "delegate Method 01"
+    }
+    
+    public class func classMethod(print aString: String) {
+        print("classMethod: \(aString)")
+    }
+    
+    public func setInsVaribale01(set aVar: String) {
+        instanceVariable01 = aVar
+    }
+    
+    public func getInsVaribale01() -> String {
+        return instanceVariable01
+    }
+    
+}
